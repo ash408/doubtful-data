6b2987881f3dffe91cff74787fe0170059dfa131
ash408 <124532093+ash408@users.noreply.github.com>
2024-10-14 10:58:14 -0500

    Create Event data object skeleton

---
+++ Event.java
@@ -0,0 +1,38 @@
+import java.util.Date;
+
+public class Event {
+	private String name;
+
+	private Date startDate;
+	private Date endDate;
+
+	Event(String name) {
+		this.name = name;
+	}
+
+	// PROPERTIES
+
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+	
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+}
