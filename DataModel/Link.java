b9c7b4f0f31c560190ed0560c6b11f729342390e
ash408 <124532093+ash408@users.noreply.github.com>
2024-10-14 11:44:00 -0500

    Create basic Link data model

---
+++ DataModel/Link.java
@@ -0,0 +1,31 @@
+import java.util.UUID;
+
+
+public class Link extends BaseModel {
+	UUID firstID;
+	UUID secondID;
+
+	Link(UUID firstID, UUID secondID) {
+		this.firstID = firstID;
+		this.secondID = secondID;
+	}
+
+	
+	// PROPERTIES
+
+	public UUID getFirstID() {
+		return firstID;
+	}
+
+	public void setFirstID(UUID id) {
+		firstID = id;
+	}
+
+	public UUID getSecondID() {
+		return secondID;
+	}
+
+	public void setSecondID(UUID id) {
+		secondID = id;
+	}
+}
